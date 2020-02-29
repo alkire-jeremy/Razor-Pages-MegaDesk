c061313b433705e20309b4131c8f0840e4841d94
Jeremy <35344334+alkire-jeremy@users.noreply.github.com>
2020-02-29 00:14:11 -0600

    Quote total now calculates correctly & gets saved in the database.

--- RazorMegaDesk/RazorMegaDesk/Pages/DeskQuotes/Create.cshtml.cs
+++ RazorMegaDesk/RazorMegaDesk/Pages/DeskQuotes/Create.cshtml.cs
@@ -12,6 +12,8 @@ namespace RazorMegaDesk.Pages.DeskQuotes
     public class CreateModel : PageModel
     {
         private readonly RazorMegaDesk.Models.RazorMegaDeskContext _context;
+        public const int SQUAREPRICE = 1;
+        public const int DRAWERPRICE = 50;
 
         public CreateModel(RazorMegaDesk.Models.RazorMegaDeskContext context)
         {
@@ -24,7 +26,7 @@ namespace RazorMegaDesk.Pages.DeskQuotes
         }
 
         [BindProperty]
-        public DeskQuote DeskQuote { get; set; }
+        public DeskQuote deskQuote { get; set; }
 
         public async Task<IActionResult> OnPostAsync()
         {
@@ -33,10 +35,106 @@ namespace RazorMegaDesk.Pages.DeskQuotes
                 return Page();
             }
 
-            _context.DeskQuote.Add(DeskQuote);
+            deskQuote.QuoteTotal = CalculateQuoteTotal(deskQuote);
+
+            _context.DeskQuote.Add(deskQuote);
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
+        }
+
+        public double CalculateQuoteTotal(DeskQuote quote)
+        {
+            double rushPrice = 0;
+            double materialPrice = 0;
+            double surfaceArea = quote.Width* quote.Depth;
+
+            List<int> materialPriceList = new List<int>() { 200, 100, 50, 300, 125 };
+
+            // Calculate materialPrice.
+            if (quote.DeskMaterial == "Oak")
+            {
+                materialPrice = materialPriceList[0];
+            }
+            else if (quote.DeskMaterial == "Laminate")
+            {
+                materialPrice = materialPriceList[1];
+            }
+            else if (quote.DeskMaterial == "Pine")
+            {
+                materialPrice = materialPriceList[2];
+            }
+            else if (quote.DeskMaterial == "Rosewood")
+            {
+                materialPrice = materialPriceList[3];
+            }
+            else if (quote.DeskMaterial == "Veneer")
+            {
+                materialPrice = materialPriceList[4];
+            }
+
+            double[] rushPrices = {60, 70, 80, 40, 50, 60, 30, 35, 40};
+
+            // Calculate rushPrice.
+            switch (quote.RushDays)
+            {
+                case 3:
+                    if (surfaceArea < 1000)
+                    {
+                        rushPrice = rushPrices[0];
+                    }
+                    else if (surfaceArea >= 1000 && surfaceArea <= 2000)
+                    {
+                        rushPrice = rushPrices[1];
+                    }
+                    else if (surfaceArea > 2000)
+                    {
+                        rushPrice = rushPrices[2];
+                    }
+                    break;
+                case 5:
+                    if (surfaceArea < 1000)
+                    {
+                        rushPrice = rushPrices[3];
+                    }
+                    else if (surfaceArea >= 1000 && surfaceArea <= 2000)
+                    {
+                        rushPrice = rushPrices[4];
+                    }
+                    else if (surfaceArea > 2000)
+                    {
+                        rushPrice = rushPrices[5];
+                    }
+                    break;
+                case 7:
+                    if (surfaceArea < 1000)
+                    {
+                        rushPrice = rushPrices[6];
+                    }
+                    else if (surfaceArea >= 1000 && surfaceArea <= 2000)
+                    {
+                        rushPrice = rushPrices[7];
+                    }
+                    else if (surfaceArea > 2000)
+                    {
+                        rushPrice = rushPrices[8];
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Something probably went wrong.");
+                    break;
+            }
+
+            if (surfaceArea > 1000)
+            {
+                double total = (surfaceArea * SQUAREPRICE) + (quote.NumberOfDrawers * DRAWERPRICE) + rushPrice + materialPrice + 200;
+                return total;
+            }
+            else
+            {
+                double total = (quote.NumberOfDrawers * DRAWERPRICE) + rushPrice + materialPrice + 200;
+                return total;
+            }
         }
     }
 }
